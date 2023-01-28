07668126e5db1fb61452534db24743dbdf609ba2
Tanya <tanyadiit7@gmail.com>
2023-01-28 14:11:36 +0530

    finished protecting routes

--- client/src/App.js
+++ client/src/App.js
@@ -67,6 +67,31 @@ function App() {
     return user ? (children) : <Navigate to="/home" />
   }
 
+  // if user is not admin redirects to login page
+  const RequireAdmin = ({ children }) => {
+    return user.isAdmin ? (children) : <Navigate to="/home" />
+  }
+
+  // if user is not student redirects to login page
+  const RequireStudent = ({ children }) => {
+    return user.isStudent ? (children) : <Navigate to="/home" />
+  }
+
+  // if user is not faculty redirects to login page
+  const RequireFaculty = ({ children }) => {
+    return user.isFaculty ? (children) : <Navigate to="/home" />
+  }
+
+  // if user is not admin then they cannot access faculty/login pages
+  const RequireCommon = ({children}) => {
+    return user.isAdmin ? <Navigate to="/home" /> : (children) 
+  }
+
+  // if user is not a creator (i.e.) cr and faculty both have access 
+  const RequireCreator = ({children}) => {
+    return user.isStudent && !user.isCr ? <Navigate to="/home" /> : (children) 
+  }
+
   return (
 
     // darkmode context
@@ -95,7 +120,9 @@ function App() {
 
         <Route path="/admin" element={
             <RequireAuth>
+                <RequireAdmin>
                 <Home type="Admin" />
+                </RequireAdmin>
             </RequireAuth>
         } />
 
@@ -106,14 +133,18 @@ function App() {
         {/* list of students */}
         <Route path="/admin/students" element={
           <RequireAuth>
+            <RequireAdmin>
               <List column={studentColumns} name="Student" type="Admin" />
+            </RequireAdmin>
           </RequireAuth>
         } />
 
         {/* single page for student */}
           <Route path="/admin/students/:studentId" element={
             <RequireAuth>
-              <SingleStudent type="Admin" />
+              <RequireAdmin>
+                <SingleStudent type="Admin" />
+              </RequireAdmin>
             </RequireAuth>
         } />
 
@@ -121,14 +152,18 @@ function App() {
         {/* edit page for student */}
         <Route path="/admin/students/:studentId/edit" element={
           <RequireAuth>
-            <EditStudent title="Update Student" type="Admin" />
+            <RequireAdmin>
+              <EditStudent title="Update Student" type="Admin" />
+            </RequireAdmin>
           </RequireAuth>
         } />
 
         {/* create user student */}
         <Route path="/admin/students/new" element={
           <RequireAuth>
-            <NewStudent inputs={studentInputs} title="Add New Student" />
+            <RequireAdmin>
+              <NewStudent inputs={studentInputs} title="Add New Student" />
+            </RequireAdmin>
           </RequireAuth>
         } />
 
@@ -139,28 +174,36 @@ function App() {
         {/* list of faculties */}
         <Route path="/admin/faculties" element={
           <RequireAuth>
+            <RequireAdmin>
               <List column={facultyColumns} name="Faculty" type="Admin" />
+            </RequireAdmin>
           </RequireAuth>
         } />
 
         {/* single page for faculty */}
           <Route path="/admin/faculties/:facultyId" element={
             <RequireAuth>
-              <SingleFaculty type="Admin" />
+              <RequireAdmin>
+                <SingleFaculty type="Admin" />
+              </RequireAdmin>
             </RequireAuth>
         } />
 
         {/* edit page for student */}
         <Route path="/admin/faculties/:facultyId/edit" element={
           <RequireAuth>
-            <EditFaculty title="Update Faculty" type="Admin" />
+            <RequireAdmin>
+              <EditFaculty title="Update Faculty" type="Admin" />
+            </RequireAdmin>
           </RequireAuth>
         } />
 
         {/* create faculty */}
         <Route path="/admin/faculties/new" element={
           <RequireAuth>
-            <NewFaculty inputs={facultyInputs} title="Add New Faculty" />
+            <RequireAdmin>
+              <NewFaculty inputs={facultyInputs} title="Add New Faculty" />
+            </RequireAdmin>
           </RequireAuth>
         } />
 
@@ -169,14 +212,18 @@ function App() {
         {/* routes for updates */}
         <Route path="/admin/updates" element={
             <RequireAuth>
+              <RequireAdmin>
                 <List column={updateColumns} name="Update" type="Admin" />
+              </RequireAdmin>
             </RequireAuth>
         } />
 
         {/* edit update */}
         <Route path="/admin/updates/:updateId/edit" element={
             <RequireAuth>
+              <RequireAdmin>
                 <EditUpdate title="Edit Updates" type="Admin"/>
+              </RequireAdmin>
             </RequireAuth>
         } />
 
@@ -184,7 +231,9 @@ function App() {
         <Route
             path="/admin/updates/new" element={
                 <RequireAuth>
+                  <RequireAdmin>
                     <NewUpdate inputs={updateInputs} title="Add New Update" type="Admin"/>
+                  </RequireAdmin>
                 </RequireAuth>
             }
         />
@@ -193,10 +242,10 @@ function App() {
         {/* routes for events */}
 
         {/* list of events */}
-        <Route path="/admin/events" element={
+        {/* <Route path="/admin/events" element={
             <RequireAuth>
             </RequireAuth>
-        } />
+        } /> */}
 
 
 
@@ -217,42 +266,54 @@ function App() {
         {/* dashboard of main */}
         <Route path="/" element={
             < RequireAuth >
+              <RequireCommon>
                 <Home type="Main" />
+              </RequireCommon>
             </RequireAuth>
         } />
 
         {/* profile page for student */}
         <Route path="/students/:id" element={
             <RequireAuth>
+              <RequireStudent>
                 <SingleStudent type="Main" />
+              </RequireStudent>
             </RequireAuth>
         } />
 
         {/* edit profile page for faculty*/}
         <Route path="/students/:id/edit" element={
             <RequireAuth>
+              <RequireStudent>
                 <EditStudent title="Edit Profile" type="Main" />
+              </RequireStudent>
             </RequireAuth>
         } />
 
         {/* profile page for faculty */}
         <Route path="/faculties/:id" element={
             <RequireAuth>
+              <RequireFaculty>
                 <SingleFaculty type="Main" />
+              </RequireFaculty>
             </RequireAuth>
         } />
 
         {/* edit profile page for faculty */}
         <Route path="/faculties/:id/edit" element={
             <RequireAuth>
+              <RequireFaculty>
                 <EditFaculty title="Edit Profile" type="Main" />
+              </RequireFaculty>
             </RequireAuth>
         } />
 
         {/* tasks page student side */}
         <Route path="/tasks" element={
             <RequireAuth>
+              <RequireStudent>
                 < List column={taskColumns} type="Main" name="Task" />
+              </RequireStudent>
             </RequireAuth>
         } />
 
@@ -262,51 +323,65 @@ function App() {
         {/* list of tasks */}
         <Route path="/tasks/cr" element={
             <RequireAuth>
+              <RequireFaculty>
                 <List column={taskColumns} name="Task" type="Creator" />
+              </RequireFaculty>
             </RequireAuth>
         } />
 
         {/* edit page for tasks */}
         <Route path="/tasks/cr/:taskId/edit" element={
             <RequireAuth>
+              <RequireFaculty>
                 <EditTask title="Update Task" />
+              </RequireFaculty>
             </RequireAuth>
         } />
 
         {/* create task page */}
         <Route path="/tasks/cr/new" element={
             <RequireAuth>
+              <RequireFaculty>
                 <NewTask inputs={taskInputs} title="Add New Task" />
+              </RequireFaculty>
             </RequireAuth>
         } />
 
         {/* updates page student side*/}
         <Route path="/updates" element={
             <RequireAuth>
+              <RequireStudent>
                 < List column={updateColumns} type="Main" name="Update" />
+              </RequireStudent>
             </RequireAuth>
         } />
 
         {/* updates page faculty side*/}
         <Route path="/updates/cr" element={
             <RequireAuth>
+              <RequireCreator>
                 < List column={updateColumns} type="Creator" name="Update" />
+              </RequireCreator>
             </RequireAuth>
         } />
 
         {/* edit update */}
         <Route path="/updates/cr/:updateId/edit" element={
             <RequireAuth>
+              <RequireCreator>
                 <EditUpdate title="Edit Updates" type="Main" />
+              </RequireCreator>
             </RequireAuth>
         } />
 
         {/* create update page */}
         <Route
             path="/updates/cr/new" element={
-                <RequireAuth>
-                    <NewUpdate inputs={updateInputs} title="Add New Update" type="Main" />
-                </RequireAuth>
+              <RequireAuth>
+                <RequireCreator>
+                  <NewUpdate inputs={updateInputs} title="Add New Update" type="Main" />
+                </RequireCreator>
+              </RequireAuth>
             }
         />
 
